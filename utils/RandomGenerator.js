81831ddb90848cb3ddcea4418e85dcdecb5306e7
Dm23_G <danna.gala@utec.edu.pe>
2026-06-28 19:38:23 -0500

    Add gaussianRandom function to RandomGenerator

--- utils/RandomGenerator.js
+++ utils/RandomGenerator.js
@@ -1,1 +1,13 @@
+// utils/RandomGenerator.js
 
+export function gaussianRandom(){
+
+    let u = 0;
+    let v = 0;
+
+    while(u===0) u=Math.random();
+    while(v===0) v=Math.random();
+
+    return Math.sqrt(-2*Math.log(u))*Math.cos(2*Math.PI*v);
+
+}
