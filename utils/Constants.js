91999d36036e4e2aa9bb7656f4c76e0aa7f77dd3
Dm23_G <danna.gala@utec.edu.pe>
2026-06-28 19:37:57 -0500

    Add constants for particle simulation settings

--- utils/Constants.js
+++ utils/Constants.js
@@ -1,1 +1,16 @@
+// utils/Constants.js
 
+export const WIDTH = 660;
+export const HEIGHT = 420;
+
+export const DEFAULT_PARTICLES = 500;
+
+export const DEFAULT_SPEED = 1.5;
+
+export const DEFAULT_RADIUS = 4;
+
+export const DEFAULT_CAPACITY = 4;
+
+export const DEFAULT_NEIGHBOR_RADIUS = 40;
+
+export const MAX_PARTICLES = 10000;
